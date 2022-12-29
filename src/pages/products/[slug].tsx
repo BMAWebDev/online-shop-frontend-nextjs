9bb0f90e578430ed4ab6439165f66d539ad603df
Bardita Andrei <bmihaiandrei@gmail.com>
2022-12-30 00:38:01 +0200

    fixed build issue

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from "react";
 
 export default function Product(): ReactElement {
-  return <>single product page. this page is where you'll buy the product.</>;
+  return (
+    <>single product page. this page is where you&apos;ll buy the product.</>
+  );
 }
